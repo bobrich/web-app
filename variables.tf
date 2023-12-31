d2759c6c114fc0bef756169e2a09ba7a9364bf70
bobrich <bobrich@gmail.com>
2023-12-30 20:31:07 -0500

    more fixin

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "github_token" {
   description = "GitHub token for AWS CodePipeline"
   type        = string
+  default     = ""
 }
 
 variable "terraform_state_bucket" {
   description = "S3 bucket for storing Terraform state files"
   type        = string
+  default     = ""
 }
